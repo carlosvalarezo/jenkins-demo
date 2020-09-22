2b031a480e8e56400bc407cdc74d9faba8e50bfa
carlosvalarezo <carlos.valarezo@gmail.com>
2020-09-22 18:06:56 -0500

    [@carlosvalarezo] adds sudo

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -3,10 +3,10 @@ groupadd ubuntu
 useradd -g ubuntu -G admin -s /bin/bash -d /home/ubuntu ubuntu
 mkdir -p /home/ubuntu
 sudo cp -r /root/.ssh /home/ubuntu/.ssh
-chown -R ubuntu:ubuntu /home/ubuntu
-echo "ubuntu ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+sudo chown -R ubuntu:ubuntu /home/ubuntu
+sudo echo "ubuntu ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 # create .kube/config
-mkdir -p ~ubuntu/.kube
-cp -i /etc/kubernetes/admin.conf ~ubuntu/.kube/config
-chown ubuntu:ubuntu ~ubuntu/.kube/config
+sudo mkdir -p ~ubuntu/.kube
+sudo cp -i /etc/kubernetes/admin.conf ~ubuntu/.kube/config
+sudo chown ubuntu:ubuntu ~ubuntu/.kube/config
