169697f7edb08c1c347a76cbecc20eb93df3486d
carlosvalarezo <carlos.valarezo@gmail.com>
2020-09-22 17:56:52 -0500

    [@carlosvalarezo] forces folders creation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "👨‍💻setting up EC2 master"
+ssh -i ./resources/aws_k8s.pem $EC2_USER@$MASTER_NODE 'mkdir' '/home/ubuntu/scripts'
+ssh -i ./resources/aws_k8s.pem $EC2_USER@$MASTER_NODE 'mkdir' '/home/ubuntu/certs'
 scp -i ./resources/aws_k8s.pem ./resources/aws_k8s.pem $EC2_USER@$MASTER_NODE:/home/ubuntu/certs/aws_k8s.pem
 scp -i ./resources/aws_k8s.pem ./scripts/start-master.sh $EC2_USER@$MASTER_NODE:/home/ubuntu/scripts/start-master.sh
 scp -i ./resources/aws_k8s.pem ./env.sh $EC2_USER@$MASTER_NODE:/home/ubuntu/scripts/env.sh
@@ -9,6 +11,7 @@ ssh -i ./resources/aws_k8s.pem $EC2_USER@$MASTER_NODE 'sh' '/home/ubuntu/scripts
 echo "👨‍ EC2 master working"
 
 echo "👨‍💻setting up EC2 node"
+ssh -i ./resources/aws_k8s.pem $EC2_USER@$MASTER_NODE 'mkdir' '/home/ubuntu/scripts'
 scp -i ./resources/aws_k8s.pem ./scripts/start-node.sh $EC2_USER@$WORKER_NODE:/home/ubuntu/scripts/start-node.sh
 ssh -i ./resources/aws_k8s.pem $EC2_USER@$WORKER_NODE 'sh' '/home/ubuntu/scripts/start-node.sh'
 echo "👨‍ EC2 node working"
