682812af8ad234dfb736d1491bb8543fe9dd23e3
carlosvalarezo <carlos.valarezo@gmail.com>
2020-09-22 18:29:25 -0500

    [@carlosvalarezo] removes systemcl commands

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -4,7 +4,6 @@ useradd -g ubuntu -G admin -s /bin/bash -d /home/ubuntu ubuntu
 mkdir -p /home/ubuntu
 sudo cp -r /root/.ssh /home/ubuntu/.ssh
 sudo chown -R ubuntu:ubuntu /home/ubuntu
-sudo cat /etc/sudoers
 sudo echo "ubuntu ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 # create .kube/config
