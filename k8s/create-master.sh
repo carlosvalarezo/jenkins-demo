eb1f74aa4f9f09b362042f92062abc1c125c63a0
carlosvalarezo <carlos.valarezo@gmail.com>
2020-09-22 18:40:08 -0500

    [@carlosvalarezo] updates scripts

--- k8s/create-master.sh
+++ k8s/create-master.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "😺 creating k8s master"
-sudo source /home/ubuntu/scripts/env.sh
+source /home/ubuntu/scripts/env.sh
+ssh -i ./resources/aws_k8s.pem $EC2_USER@$MASTER_NODE 'mkdir' '/home/ubuntu/worker_node'
 
 JOIN=$(kubeadm init --pod-network-cidr=10.244.0.0/16)
 
 sudo echo $JOIN > /etc/kubernetes/join.conf
 
-sudo scp -i /home/ubuntu/certs/aws_k8s.pem /etc/kubernetes/join.conf $EC2_USER@$WORKER_NODE:/home/ubuntu/worker_node/join.conf
+scp -i /home/ubuntu/certs/aws_k8s.pem /etc/kubernetes/join.conf $EC2_USER@$WORKER_NODE:/home/ubuntu/worker_node/join.conf
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
